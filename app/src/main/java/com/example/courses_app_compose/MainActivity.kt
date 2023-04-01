fe863e3a6c0f65ded1bb6e59d59e46eb5d704a38
Sourabh Kumar <20010001030@gateway.edu.in>
2023-04-01 22:55:00 +0530

    added topicGrid and preview

--- app/src/main/java/com/example/courses_app_compose/MainActivity.kt
+++ app/src/main/java/com/example/courses_app_compose/MainActivity.kt
@@ -1,22 +1,27 @@
 package com.example.courses_app_compose
 
-import android.content.ClipData.Item
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.courses_app_compose.data.DataSource
 import com.example.courses_app_compose.model.Topic
 import com.example.courses_app_compose.ui.theme.Courses_App_ComposeTheme
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -28,7 +33,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    TopicGrid()
                 }
             }
         }
@@ -88,10 +93,31 @@ fun TopicCard(
     }
 }
 
-@Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
+fun TopicGrid(modifier: Modifier = Modifier) {
+    LazyVerticalGrid(
+        columns = GridCells.Fixed(2),
+        verticalArrangement = Arrangement.spacedBy(8.dp),
+        horizontalArrangement = Arrangement.spacedBy(8.dp)
+    ) {
+        items(DataSource.topics) { topic ->
+            TopicCard(topic)
+        }
+    }
+}
+
+@Preview
+@Composable
+fun TopicPreview(){
     Courses_App_ComposeTheme {
-        Greeting("Android")
+        val topic = Topic(R.string.crafts,313,R.drawable.ic_grain)
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            TopicCard(topic = topic)
+        }
+
     }
 }
